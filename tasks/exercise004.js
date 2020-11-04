107a576d608eb535586209ebd8228c2da98aba26
vahed <vahe@hotmail.co.uk>
2020-11-04 17:42:14 +0000

    another commit

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -1,29 +1,17 @@
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+// Leave punctuation marks untouched.
 
 function pigLatin(str){
     // You code here!
-    let firstChar = [];
-    let lastCharOfWord = [];
-    let concatEndOfWord = [];
+    var newStr = str.split(/(\W+)/);
 
-    newstr = str.split(" ");
+    var changed = newStr.map(function(input) {
+        if (!/\w/.test(input)) return input // keep non word elements as is
 
-    for(i=0; i<newstr.length; i++){
-        firstChar.push(newstr[i].charAt(0));
-        lastCharOfWord.push(newstr[i].charAt(newstr[i].length-1));
-        concatEndOfWord.push(newstr[i].charAt(0).concat('ay'));
-    }
-
-    let slicedArr = newstr.map(value => value.slice(1));
-
-    let result = slicedArr.map(function (value, index) {
-        return value + String(concatEndOfWord[index])
-    })
-
-    var newArray = result.reduce(function(a, b) {
-        return a + " " + b;
+        return input.substring(1) + input.charAt(0) + "ay";
     });
-    return console.log(String(newArray));
+
+    return changed.join("");
 }
 
 module.exports = {
